d362f1b3abe5f7dd80ab211b54907eed540a4414
ej68 <ej68@duke.edu>
2018-06-20 15:10:42 -0400

    new approach with voronoi maps for each floor of perkins library

---
+++ perkinsApp/perkins2/app.R
@@ -0,0 +1,70 @@
+library(shiny)
+library(readr)
+library(ggplot2)
+library(png)
+library(dplyr)
+library(plotly)
+library(lubridate)
+library(deldir)
+library(sp)
+library(rgeos)
+library(raster)
+
+ap_xy <- read_csv("../perkins_aps")
+walls <- read_csv("../perkins_walls")
+events <- read_csv("../../perkinsOnly.csv")
+aps <- data.frame("ap" = unique(events$ap))
+aps <- merge(aps, ap_xy)
+events <- merge(events, aps)
+
+# Creating polygons for the walls of the library.
+walls0 <- SpatialPolygons(list(Polygons(list(Polygon(walls[walls$floor == 0, c("transX", "transY")])),1)))
+walls1 <- SpatialPolygons(list(Polygons(list(Polygon(walls[walls$floor == 1, c("transX", "transY")])),1)))
+walls2 <- SpatialPolygons(list(Polygons(list(Polygon(walls[walls$floor == 2, c("transX", "transY")])),1)))
+walls3 <- SpatialPolygons(list(Polygons(list(Polygon(walls[walls$floor == 3, c("transX", "transY")])),1)))
+wallList <- list(walls0, walls1, walls2, walls3)
+
+# Creating voronoi cells for each floor.
+cells0 <- deldir(aps[aps$floor == 0, c("x" = "transX", "y" = "transY")])
+cells1 <- deldir(aps[aps$floor == 1, c("x" = "transX", "y" = "transY")])
+cells2 <- deldir(aps[aps$floor == 2, c("x" = "transX", "y" = "transY")])
+cells3 <- deldir(aps[aps$floor == 3, c("x" = "transX", "y" = "transY")])
+
+# Convert cell info to spatial data frames (polygons)
+ws <- list(tile.list(cells0), tile.list(cells1), tile.list(cells2), tile.list(cells3))
+SPDFlist <- lapply(0:3, function(i){
+  w <- ws[[i+1]]
+  sps <- wallList[[i+1]]
+  polys <- vector(mode="list", length=length(w))
+  for (j in seq(along=polys)) {
+    pcrds <- cbind(w[[j]]$x, w[[j]]$y)
+    pcrds <- rbind(pcrds, pcrds[1,])
+    polys[[j]] <- Polygons(list(Polygon(pcrds)), ID=as.character(j))
+  }
+  SP <- SpatialPolygons(polys)
+  SP <- intersect(SP, sps)
+  for(x in 1:nrow(aps[aps$floor == i,])){
+    SP@polygons[[x]]@ID <- as.character(x)
+  }
+  SPDF <- SpatialPolygonsDataFrame(SP, data=data.frame(
+    x = aps[aps$floor == i,"transX"], 
+    y = aps[aps$floor == i,"transY"]))
+  # tag polygons with ap name
+  SPDF@data$ID = as.character(aps[aps$floor == i, "ap"])
+  sapply(1:length(aps[aps$floor == i, "ap"]), function(x){
+    SPDF@polygons[[x]]@ID <- as.character(aps[aps$floor == i, "ap"][x])
+    SPDF <<- SPDF
+  })
+  return(SPDF)
+})
+
+
+ui <- fluidPage(
+)
+
+server <- function(input, output) {
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
